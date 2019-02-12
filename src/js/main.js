479c193120c52bd1a94b7e522cabc9ff403495f3
AnatoliyZ <zolotukhinwebpr@mail.ru>
2019-02-12 22:52:04 +0400

    header + navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 /* Main js */
+
+/**
+ * Закрепленное к верху меню навигации
+ */
+const fixedNavigationMenu = () => {
+  const navigation = document.querySelector('.navigation');
+  window.onscroll = () => {
+    if(window.pageYOffset > 96) {
+      navigation.style.position = 'fixed';
+      navigation.style.backgroundColor = 'rgba(255,255,255,0.9)';
+      navigation.style.top = '0';
+    } else if (window.pageYOffset < 30) {
+      navigation.style.backgroundColor = '';
+      navigation.style.position = 'relative';
+    }
+  }
+};
+fixedNavigationMenu();
